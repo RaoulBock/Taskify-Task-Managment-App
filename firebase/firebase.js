6d2488c7b51a40df41f7295215c17564065337e3
Raoul Bock <raoulb@deepcatch.com.na>
2024-09-05 12:16:32 +0200

    changes

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -2,15 +2,23 @@
 import { initializeApp } from "firebase/app";
 import auth from "@react-native-firebase/auth"; // Import from @react-native-firebase/auth
 import { getFirestore } from "firebase/firestore";
+import {
+  FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_PROFILE_ID,
+  FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID,
+} from "@env";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCLButpzUOPnneotrp-3PGJK19wZ6K3F84",
-  authDomain: "auth-development-e9e5a.firebaseapp.com",
-  projectId: "auth-development-e9e5a",
-  storageBucket: "auth-development-e9e5a.appspot.com",
-  messagingSenderId: "1084922291104",
-  appId: "1:1084922291104:web:7bbbcb9f6316cad8233ac5",
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROFILE_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
